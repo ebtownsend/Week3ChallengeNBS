c7bf9375534afc6a9554e241eceec4b5d15a08ab
elle <elle.townsend@nationwide.co.uk>
2020-10-01 17:14:03 +0100

    code completed

---
+++ DigitToWord/DigitToWord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitToWord
+{
+    class DigitToWord
+    {
+        public string digitToWord(int number)
+        {
+            string[] units = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+            string[] tens = { "zero", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+            string words = "";
+
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            if ((number / 1000) > 0) // get thousands digit
+            {
+                words += units[(number / 1000)] + " thousand ";
+                number = number % 1000; // take off thousands, keep remainder.
+            }
+
+            if ((number / 100) > 0) // get hundreds digit
+            {
+                words += units[(number / 100)] + " hundred ";
+                number = number % 100; // take off hundreds, keep remainder.
+            }
+
+            if (number > 0)
+            {
+                if(words != "")
+                {
+                    // if not empty, it must have hundreds value.
+                    words += "and ";
+                }
+
+                if (number < 20)
+                {
+                    words += units[number];
+                } else
+                {
+                    // divide by ten to get 'tens' number
+                    words += tens[number / 10];
+
+                    if ((number % 10) > 0) // modulus to get remainder
+                    {
+                        words += " " + units[number % 10];
+                    }
+                } 
+            }
+            return words;
+        }
+    }
+}
---
+++ DigitToWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitToWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitToWord dtw = new DigitToWord();
+            Console.WriteLine(dtw.digitToWord(7));
+            Console.WriteLine(dtw.digitToWord(356));
+            Console.WriteLine(dtw.digitToWord(999));
+            Console.WriteLine(dtw.digitToWord(3781));
+            Console.WriteLine(dtw.digitToWord(9999));
+
+            Console.ReadKey();
+        }
+    }
+}
